bd72347ed7ca15c4f84ba1bb17b1fea456e37109
Ashton Hellwig <ahellwig.dev@gmail.com>
2018-01-25 17:32:30 -0700

    removed cmake

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ extern crate iron;
 extern crate staticfile;
 extern crate mount;
 
-// This example serves the docs from target/doc/staticfile at /doc/
+// Simple static file server written in rust and eventually
+// Elm too!
 //
-// Run `cargo doc && cargo run --example doc_server`, then
-// point your browser to http://127.0.0.1:3000/doc/
+// Run `cargo run` then
+// Navigate browser to http://127.0.0.1:3000/home/
 
 use std::path::Path;
 
@@ -16,12 +17,13 @@ use mount::Mount;
 fn main() {
     let mut mount = Mount::new();
 
-    // Serve the shared JS/CSS at /public/src/
+    // Serve the shared JS/CSS at /public/javascript/
     mount.mount("/", Static::new(Path::new("public/javascript/")));
-    mount.mount("/", Static::new(Path::new("public/")));
-    // Serve the static file docs at /doc/
-    mount.mount("/home/", Static::new(Path::new("target/doc/staticfile/")));
 
+    // Serve the static file docs at /home/
+    mount.mount("/home/", Static::new(Path::new("public/")));
+
+    // Log
     println!("Web server running on http://localhost:3000/home/");
 
     Iron::new(mount).http("127.0.0.1:3000").unwrap();

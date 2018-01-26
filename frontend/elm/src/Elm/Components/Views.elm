e9e38adbfcaa08e86d8fbf4f3dcfdb3e427835ef
Ashton Hellwig <ahellwig.dev@gmail.com>
2018-01-25 18:02:20 -0700

    modularize elm

--- frontend/elm/src/Elm/Components/Views.elm
+++ frontend/elm/src/Elm/Components/Views.elm
@@ -1,72 +1,14 @@
-{- This file re-implements the Elm Counter example (1 counter) with elm-mdl
-   buttons. Use this as a starting point for using elm-mdl components in your own
-   app.
--}
+module Components.Views exposing (..)
 
-
-module Main exposing (..)
-
+import Components.Models exposing (..)
+import Components.Types exposing (..)
+import Components.Update exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (class, href, style)
 import Material
 import Material.Button as Button
 import Material.Options as Options exposing (css)
 import Material.Scheme
-
-
--- MODEL
-
-
-type alias Model =
-    { count : Int
-    , mdl :
-        Material.Model
-
-    -- Boilerplate: model store for any and all Mdl components you use.
-    }
-
-
-model : Model
-model =
-    { count = 0
-    , mdl =
-        Material.model
-
-    -- Boilerplate: Always use this initial Mdl model store.
-    }
-
-
-
--- ACTION, UPDATE
-
-
-type Msg
-    = Increase
-    | Reset
-    | Mdl (Material.Msg Msg)
-
-
-
--- Boilerplate: Msg clause for internal Mdl messages.
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        Increase ->
-            ( { model | count = model.count + 1 }
-            , Cmd.none
-            )
-
-        Reset ->
-            ( { model | count = 0 }
-            , Cmd.none
-            )
-
-        -- Boilerplate: Mdl action handler.
-        Mdl msg_ ->
-            Material.update Mdl msg_ model
-
 
 
 -- VIEW
@@ -120,13 +62,3 @@ view model =
 -- Load Google Mdl CSS. You'll likely want to do that not in code as we
 -- do here, but rather in your master .html file. See the documentation
 -- for the `Material` module for details.
-
-
-main : Program Never Model Msg
-main =
-    Html.program
-        { init = ( model, Cmd.none )
-        , view = view
-        , subscriptions = always Sub.none
-        , update = update
-        }
